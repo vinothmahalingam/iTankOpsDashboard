64ce032c3b9500bc6b0bde98fa5da311f98a0412
Sunnyat Ahmmed <myplaneticket@gmail.com>
2017-06-26 06:48:36 -0400

    update
    Signed-off-by: Sunnyat Ahmmed <myplaneticket@gmail.com>

--- src/js/_modules/app.ngmenu.js
+++ src/js/_modules/app.ngmenu.js
@@ -26,111 +26,126 @@ $.fn.extend({
              **/
             $this = $(this);
 
-        /**
-         * add a mark [+] to a multilevel menu
-         **/
-        $this.find("li").each(function() {
-            if ($(this).find("ul").length !== 0) {
+        if (!$this.hasClass("ngmenu-built")) {
 
-                /**
-                 * add the multilevel sign next to the link
-                 **/
-                $(this).find("a:first").append("<b class='collapse-sign'>" + opts.closedSign + "</b>");
+            /**
+             * confirm build to prevent rebuild error
+             **/
+            $this.addClass("ngmenu-built");
 
-                /**
-                 * avoid jumping to the top of the page when the href is an #
-                 **/
-                if ($(this).find("a:first").attr('href') == "#") {
-                    $(this).find("a:first").click(function() {
-                        return false;
-                    });
-                }
-            }
-        });
-
-        //add open sign to all active lists
-        $this.find("li.active").each(function() {
-            $(this).parents("ul")
-            	.parent("li")
-                //.addClass("open") [disabled: creates a wierd jumping effect]
-            	.find("a:first")
-            	.attr('aria-expanded', true)
-            	.find("b:first")
-            	.html(opts.openedSign);
-        });
-
-        $this.find("li a").on('mousedown', function(e) {
-
-            if ($(this).parent().find("ul").length !== 0) {
-
-                if (opts.accordion) {
+            /**
+             * add a mark [+] to a multilevel menu
+             **/
+            $this.find("li").each(function() {
+                if ($(this).find("ul").length !== 0) {
 
                     /**
-                     * Do nothing when the list is open
+                     * add the multilevel sign next to the link
                      **/
-                    if (!$(this).parent().find("ul").is(':visible')) {
+                    $(this).find("a:first").append("<b class='collapse-sign'>" + opts.closedSign + "</b>");
 
-                        parents = $(this).parent().parents("ul");
-                        visible = $this.find("ul:visible");
-                        visible.each(function(visibleIndex) {
-                            var close = true;
-                            parents.each(function(parentIndex) {
+                    /**
+                     * avoid jumping to the top of the page when the href is an #
+                     **/
+                    if ($(this).find("a:first").attr('href') == "#") {
+                        $(this).find("a:first").click(function() {
+                            return false;
+                        });
+                    }
+                }
+            });
 
-                                if (parents[parentIndex] == visible[visibleIndex]) {
+            //add open sign to all active lists
+            $this.find("li.active").each(function() {
+                $(this).parents("ul")
+                	.parent("li")
+                    //.addClass("open") [disabled: creates a wierd jumping effect]
+                	.find("a:first")
+                	.attr('aria-expanded', true)
+                	.find("b:first")
+                	.html(opts.openedSign);
+            });
 
-                                    close = false;
-                                    return false;
+            //click events
+            $this.find("li a").on('mousedown', function(e) {
+
+                if ($(this).parent().find("ul").length !== 0) {
+
+                    if (opts.accordion) {
+
+                        /**
+                         * Do nothing when the list is open
+                         **/
+                        if (!$(this).parent().find("ul").is(':visible')) {
+
+                            parents = $(this).parent().parents("ul");
+                            visible = $this.find("ul:visible");
+                            visible.each(function(visibleIndex) {
+                                var close = true;
+                                parents.each(function(parentIndex) {
+
+                                    if (parents[parentIndex] == visible[visibleIndex]) {
+
+                                        close = false;
+                                        return false;
+                                    }
+                                });
+                                if (close) {
+
+                                    if ($(this).parent().find("ul") != visible[visibleIndex]) {
+
+                                        $(visible[visibleIndex]).slideUp(opts.speed + 300, opts.animate, function() {
+                                            $(this).parent("li")
+                                            	.removeClass("open")
+                                            	.find("a:first")
+                                            	.attr('aria-expanded', false)
+                                            	.find("b:first")
+                                            	.html(opts.closedSign);
+                                        });
+                                    }
                                 }
                             });
-                            if (close) {
+                        }
+                    }
 
-                                if ($(this).parent().find("ul") != visible[visibleIndex]) {
+                    /**
+                     * Add active class to open element
+                     **/
+                    if ($(this).parent().find("ul:first").is(":visible") && !$(this).parent().find("ul:first").hasClass("active")) {
 
-                                    $(visible[visibleIndex]).slideUp(opts.speed + 300, opts.animate, function() {
-                                        $(this).parent("li")
-                                        	.removeClass("open")
-                                        	.find("a:first")
-                                        	.attr('aria-expanded', false)
-                                        	.find("b:first")
-                                        	.html(opts.closedSign);
-                                    });
-                                }
+                        $(this).parent().find("ul:first").slideUp(opts.speed + 100, opts.animate, function() {
+                            $(this).parent("li")
+                            	.removeClass("open")
+                            	.find("a:first")
+                            	.attr('aria-expanded', false)
+                            	.find("b:first").delay(opts.speed)
+                            	.html(opts.closedSign);
+                        });
+                    } else {
+                        $(this).parent().find("ul:first").slideDown(opts.speed, opts.animate, function() {
+
+                            $(this).parent("li")
+                            	.addClass("open")
+                            	.find("a:first")
+                            	.attr('aria-expanded', true)
+                            	.find("b:first").delay(opts.speed)
+                            	.html(opts.openedSign);
+
+                            /*bug fixed: addresses the .mod-main-boxed class bug, when nav exceeds content height*/
+                            if (myapp_config.root_.hasClass("mod-main-boxed")) {
+                                initApp.fixAppHeight();
                             }
                         });
                     }
                 }
+            });    
 
-                /**
-                 * Add active class to open element
-                 **/
-                if ($(this).parent().find("ul:first").is(":visible") && !$(this).parent().find("ul:first").hasClass("active")) {
+        } else {
+            
+            if (myapp_config.debugState)
+                console.log(" this menu already exists");       
+        }
 
-                    $(this).parent().find("ul:first").slideUp(opts.speed + 100, opts.animate, function() {
-                        $(this).parent("li")
-                        	.removeClass("open")
-                        	.find("a:first")
-                        	.attr('aria-expanded', false)
-                        	.find("b:first").delay(opts.speed)
-                        	.html(opts.closedSign);
-                    });
-                } else {
-                    $(this).parent().find("ul:first").slideDown(opts.speed, opts.animate, function() {
-
-                        $(this).parent("li")
-                        	.addClass("open")
-                        	.find("a:first")
-                        	.attr('aria-expanded', true)
-                        	.find("b:first").delay(opts.speed)
-                        	.html(opts.openedSign);
-
-                        /*bug fixed: addresses the .mod-main-boxed class bug, when nav exceeds content height*/
-                        if (myapp_config.root_.hasClass("mod-main-boxed")) {
-                            initApp.fixAppHeight();
-                        }
-                    });
-                }
-            }
-        });
     },
 
     destroy: function() {
