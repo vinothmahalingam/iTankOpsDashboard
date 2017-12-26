8ec2fdec3400589aeae775e1dd183151fe33699a
Sunnyat Ahmmed <myplaneticket@gmail.com>
2017-12-26 04:38:53 -0500

    nightly build

--- src/custom/plugins/jquery-snippets/jquery-snippets.js
+++ src/custom/plugins/jquery-snippets/jquery-snippets.js
@@ -18,3 +18,23 @@ $.fn.removeClassPrefix = function (prefix) {
 
     return this;
 };
+
+/**
+ * "http://dummy.com/?technology=jquery&blog=jquerybyexample". 
+ * 1 var tech = GetURLParameter('technology');
+ * 2 var blog = GetURLParameter('blog');
+ */
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
